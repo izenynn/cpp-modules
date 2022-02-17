413ba050f39bfba454474194079afc207d7713ab
izenynn <me@izenynn.com>
2022-02-17 12:11:34 +0100

    add todo

--- cpp-module-04/ex03/main.cpp
+++ cpp-module-04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dpoveda- <me@izenynn.com>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 02:56:42 by dpoveda-          #+#    #+#             */
-/*   Updated: 2022/02/17 03:15:11 by dpoveda-         ###   ########.fr       */
+/*   Updated: 2022/02/17 03:21:22 by dpoveda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,27 @@
 #include "Ice.hpp"
 #include "Cure.hpp"
 
+// TODO test unequip :D
 int main()
 {
 	IMateriaSource* src = new MateriaSource();
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
+
 	ICharacter* me = new Character("me");
+
 	AMateria* tmp;
 	tmp = src->createMateria("ice");
 	me->equip(tmp);
 	tmp = src->createMateria("cure");
+
 	me->equip(tmp);
+
 	ICharacter* bob = new Character("bob");
+
 	me->use(0, *bob);
 	me->use(1, *bob);
+
 	delete bob;
 	delete me;
 	delete src;
