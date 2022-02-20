f7668c4937b8bcbe232b5f8e83f0085190e0de06
izenynn <me@izenynn.com>
2022-02-20 03:36:54 +0100

    chore: add comments to main

--- cpp-module-08/ex02/main.cpp
+++ cpp-module-08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dpoveda- <me@izenynn.com>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 03:26:47 by dpoveda-          #+#    #+#             */
-/*   Updated: 2022/02/20 03:27:10 by dpoveda-         ###   ########.fr       */
+/*   Updated: 2022/02/20 03:36:30 by dpoveda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,25 @@
 int main()
 {
 	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
+	mstack.push(5); // 5
+	mstack.push(17); // 17, 5
 	std::cout << mstack.top() << std::endl;
-	mstack.pop();
+	mstack.pop(); // 5
 	std::cout << mstack.size() << std::endl;
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
+	mstack.push(3); // 3, 5
+	mstack.push(5); // 5, 3, 5
+	mstack.push(737); // 737, 5, 3, 5
 	//[...]
-	mstack.push(0);
+	mstack.push(0); // 0, 737, 5, 3, 5
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
 	++it;
 	--it;
+	std::cout << "\nITERATE STACK:" << std::endl;
 	while (it != ite)
 	{
-	std::cout << *it << std::endl;
-	++it;
+		std::cout << *it << std::endl;
+		++it;
 	}
 	std::stack<int> s(mstack);
 	return 0;
