02307b313e1ad961111ab3ab2cf1ee37b5553cbd
izenynn <me@izenynn.com>
2022-02-20 14:50:36 +0100

    chore: improve main.cpp to test with list also

--- cpp-module-08/ex02/main.cpp
+++ cpp-module-08/ex02/main.cpp
@@ -6,37 +6,82 @@
 /*   By: dpoveda- <me@izenynn.com>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 03:26:47 by dpoveda-          #+#    #+#             */
-/*   Updated: 2022/02/20 03:36:30 by dpoveda-         ###   ########.fr       */
+/*   Updated: 2022/02/20 14:49:36 by dpoveda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mutantstack.hpp"
 
 #include <iostream>
+#include <list>
 
 int main()
 {
+	std::cout << "========== MUTANT STACK ==========" << std::endl;
+
 	MutantStack<int> mstack;
 	mstack.push(5); // 5
 	mstack.push(17); // 17, 5
-	std::cout << mstack.top() << std::endl;
+	std::cout << "Top: " << mstack.top() << std::endl;
 	mstack.pop(); // 5
-	std::cout << mstack.size() << std::endl;
+	std::cout << "Size: " << mstack.size() << std::endl;
 	mstack.push(3); // 3, 5
-	mstack.push(5); // 5, 3, 5
-	mstack.push(737); // 737, 5, 3, 5
-	//[...]
-	mstack.push(0); // 0, 737, 5, 3, 5
+	mstack.push(7); // 7, 3, 5
+	mstack.push(737); // 737, 7, 3, 5
+	mstack.push(0); // 0, 737, 7, 3, 5
+	std::cout << "Size: " << mstack.size() << std::endl;
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
 	++it;
 	--it;
 	std::cout << "\nITERATE STACK:" << std::endl;
-	while (it != ite)
-	{
+	while (it != ite) {
 		std::cout << *it << std::endl;
 		++it;
 	}
 	std::stack<int> s(mstack);
+	std::cout << "\nREVERSE ITERATE STACK:" << std::endl;
+	MutantStack<int>::reverse_iterator rit = mstack.rbegin();
+	MutantStack<int>::reverse_iterator rite = mstack.rend();
+	++rit;
+	--rit;
+	while (rit != rite) {
+		std::cout << *rit << std::endl;
+		++rit;
+	}
+
+	std::cout << "========== LIST ==========" << std::endl;
+	
+	std::list<int> ls;
+	ls.push_back(5);
+	ls.push_back(17);
+	std::cout << "Back: " << ls.back() << std::endl;
+	ls.pop_back();
+	std::cout << "Size: " << ls.size() << std::endl;
+	ls.push_back(3);
+	ls.push_back(7);
+	ls.push_back(737);
+	ls.push_back(0);
+	std::cout << "Size: " << ls.size() << std::endl;
+	std::list<int>::iterator lit = ls.begin();
+	std::list<int>::iterator lite = ls.end();
+	++lit;
+	--lit;
+	std::cout << "\nITERATE LIST:" << std::endl;
+	while (lit != lite) {
+		std::cout << *lit << std::endl;
+		++lit;
+	}
+	std::list<int> ls2(ls);
+	std::cout << "\nREVERSE ITERATE LIST" << std::endl;	
+	std::list<int>::reverse_iterator rlit = ls.rbegin();
+	std::list<int>::reverse_iterator rlite = ls.rend();
+	++rlit;
+	--rlit;
+	while (rlit != rlite) {
+		std::cout << *rlit << std::endl;
+		++rlit;
+	}
+
 	return 0;
 }
--- cpp-module-08/ex02/mutantstack.hpp
+++ cpp-module-08/ex02/mutantstack.hpp
@@ -6,7 +6,7 @@
 /*   By: dpoveda- <me@izenynn.com>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 03:00:39 by dpoveda-          #+#    #+#             */
-/*   Updated: 2022/02/20 03:36:04 by dpoveda-         ###   ########.fr       */
+/*   Updated: 2022/02/20 14:38:53 by dpoveda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define MUTANTSTACK_HPP_
 
 # include <stack>
+# include <deque>
 
 template <typename T, typename Container = std::deque<T> >
 class MutantStack : public std::stack<T, Container> {
@@ -32,12 +33,20 @@ class MutantStack : public std::stack<T, Container> {
 
 		typedef typename std::stack<T, Container>::container_type::iterator iterator;
 		typedef typename std::stack<T, Container>::container_type::const_iterator const_iterator;
+		typedef typename std::stack<T, Container>::container_type::reverse_iterator reverse_iterator;
+		typedef typename std::stack<T, Container>::container_type::const_reverse_iterator const_reverse_iterator;
 
 		iterator begin() { return this->std::stack<T, Container>::c.begin(); }
 		iterator end() { return this->std::stack<T, Container>::c.end(); }
 
+		reverse_iterator rbegin() { return this->std::stack<T, Container>::c.rbegin(); }
+		reverse_iterator rend() { return this->std::stack<T, Container>::c.rend(); }
+
 		const_iterator begin() const { return this->std::stack<T, Container>::c.begin(); }
 		const_iterator end() const { return this->std::stack<T, Container>::c.end(); }
+
+		const_reverse_iterator rbegin() const { return this->std::stack<T, Container>::c.rbegin(); }
+		const_reverse_iterator rend() const { return this->std::stack<T, Container>::c.rend(); }
 };
 
 #endif
