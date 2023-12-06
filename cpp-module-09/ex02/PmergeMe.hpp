788b542c3b748769658b9c3c9f8f703601463b96
izenynn <me@izenynn.com>
2023-12-06 13:31:49 +0100

    feat: ford johnson

--- cpp-module-09/ex02/PmergeMe.hpp
+++ cpp-module-09/ex02/PmergeMe.hpp
@@ -3,13 +3,16 @@
 
 #include <vector>
 #include <deque>
+#include <algorithm>
 
 class PmergeMe {
 public:
 	template <class T>
-	static void process(T &c, int threshold = PmergeMe::kDefaultThreshold)
+	static void process(T &c)
 	{
-		mergeInsertSort(c, 0, c.size() - 1, threshold);
+		if (!c.empty()) {
+			fordJohnsonMergeInsertionSort(c, 0, c.size() - 1);
+		}
 	}
 	
 private:
@@ -18,63 +21,95 @@ private:
 	PmergeMe(const PmergeMe &rhs);
 	PmergeMe &operator=(const PmergeMe &rhs);
 
-	static const int kDefaultThreshold = 10;
+	template <class T>
+	struct Compare {
+		const T& c;
+		Compare(const T& set) : c(set) {}
+		bool operator()(int a, int b) { return c[a] < c[b]; }
+	};
 
 	template <class T>
-	static void mergeInsertSort(T& c, int start, int end, int threshold)
+	static void fordJohnsonMergeInsertionSort(T &c, int start, int end)
 	{
-		if (start < end) {
-			if (end - start < threshold) {
-				insertSort(c, start, end);
-			} else {
-				int mid = start + (end - start) / 2;
-				mergeInsertSort(c, start, mid, threshold);
-				mergeInsertSort(c, mid + 1, end, threshold);
-				merge(c, start, mid, end);
+		if (start >= end) return; // Base case for single element or invalid range
+	
+		if (end - start == 1) {
+			if (c[start] > c[end]) {
+				std::swap(c[start], c[end]);
+			}
+			return;
+		}
+	
+		// Sort pairs and find medians
+		std::vector<int> medians;
+		for (int i = start; i < end; i += 2) {
+			if (c[i] > c[i + 1]) {
+				std::swap(c[i], c[i + 1]);
+			}
+			medians.push_back(i + 1);
+		}
+	
+		// Handle odd number of elements
+		if ((end - start + 1) % 2 != 0) {
+			medians.push_back(end);
+		}
+	
+		// Find median of medians
+		int medianOfMediansIndex = selectMedian(c, medians);
+	
+		// Partition around median of medians
+		int mid = partition(c, start, end, medianOfMediansIndex);
+	
+		// Recursively sort elements before and after the partition index
+		fordJohnsonMergeInsertionSort(c, start, mid - 1);
+		fordJohnsonMergeInsertionSort(c, mid, end);
+	}
+	
+	template <class T>
+	static int partition(T &c, int start, int end, int pivotIndex) {
+		std::swap(c[pivotIndex], c[end]); // Move pivot to end
+		int storeIndex = start;
+		for (int i = start; i < end; i++) {
+			if (c[i] < c[end]) {
+				std::swap(c[i], c[storeIndex]);
+				storeIndex++;
 			}
 		}
+		std::swap(c[storeIndex], c[end]); // Move pivot to its final place
+		return storeIndex;
+	}
+
+
+	template <class T>
+	static int selectMedian(T &c, const std::vector<int> &medians) {
+		std::vector<int> temp = medians;
+		std::sort(temp.begin(), temp.end(), Compare<T>(c));
+		return temp[temp.size() / 2];
 	}
 
 	template <class T>
-	static void insertSort(T &c, int start, int end)
-	{
-		for (int i = start + 1; i <= end; ++i) {
-			int key = c[i];
-			int j = i - 1;
-			for (; j >= start && c[j] > key; --j)
-				c[j + 1] = c[j];
-			c[j + 1] = key;
+	static void merge(T &c, int start, int median, int end) {
+		std::vector<typename T::value_type> temp;
+		temp.reserve(end - start + 1);
+	
+		int left = start, right = median + 1;
+		while (left <= median && right <= end) {
+			if (c[left] < c[right]) {
+				temp.push_back(c[left++]);
+			} else {
+				temp.push_back(c[right++]);
+			}
 		}
-	}
-
-	template <class T>
-	static void merge(T &c, int start, int mid, int end)
-	{
-		int i, j, k;
-		int n1 = mid - start + 1;
-		int n2 = end - mid;
 	
-		T left(n1), right(n2);
-	
-		for (i = 0; i < n1; ++i)
-			left[i] = c[start + i];
-		for (j = 0; j < n2; ++j)
-			right[j] = c[mid + 1 + j];
-	
-		i = 0;
-		j = 0;
-		k = start;
-		while (i < n1 && j < n2) {
-			if (left[i] <= right[j])
-				c[k++] = left[i++];
-			else
-				c[k++] = right[j++];
+		while (left <= median) {
+			temp.push_back(c[left++]);
 		}
-		while (i < n1)
-			c[k++] = left[i++];
 	
-		while (j < n2)
-			c[k++] = right[j++];
+		while (right <= end) {
+			temp.push_back(c[right++]);
+		}
+	
+		std::copy(temp.begin(), temp.end(), c.begin() + start);
 	}
 };
 
